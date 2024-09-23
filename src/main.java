43e3652a8cef7d8b57348f706525dc8a228b992b
Mikhail_Yudkin <kimopwnz@gmail.com>
2024-09-23 14:52:20 +0300

    Third commit

--- src/main.java
+++ src/main.java
@@ -3,8 +3,12 @@ public class main {
         String s1 = "Иван Иванов";
         String s2 = "иван иванов";
         s1.equals(s2);
+        System.out.println(s1);
+
+
         System.out.println();
         System.out.println();
+        System.out.println(s2);
         );
     }
 }
